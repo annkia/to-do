86bcedcc637ab6557f6f4f01d6fe82372f88c420
Anna <Anna.Olender@grandparade.co.uk>
2020-08-01 09:02:52 +0200

    create simple list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import List from "./components/List";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
-      <h1>To do list</h1>
+      <List></List>
     </div>
   );
 }
---
+++ src/components/List.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./List.css";
+
+let noteList = [
+  { id: "a", value: "Buy cat's food", completed: false },
+  { id: "b", value: "Call to sister", completed: false },
+  { id: "c", value: "Clean up bathroom", completed: false },
+];
+
+const List = () => {
+  return (
+    <div className="main-view">
+      <h1>To do list</h1>
+      <h2>Your list:</h2>
+      <h2>
+        {noteList.map((note) => (
+          <ul>
+            <li key={note.id}>{note.value}</li>
+          </ul>
+        ))}
+      </h2>
+    </div>
+  );
+};
+
+export default List;
